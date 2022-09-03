5cd2f3ca1adb8be6a0d6b495b52e7a95a3fff35b
masha <maria.lileya@gmail.com>
2022-09-03 20:10:20 +0300

    dicts

--- sets.py
+++ sets.py
@@ -15,7 +15,21 @@ print(f"{set_digits=}")
 set_a = {1, 3, 5, 7, 9}
 set_b = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
 set_c = {"s", "f", "U", "R", "r", "j", 5, 7}
-print(f"union --> {set_a | set_b | set_c}")
-print(f"intersection --> {set_a & set_b & set_c}")
-print(f"difference --> {set_b - set_c - set_a}")
-print(f"symetric_difference --> {set_a ^ set_b ^ set_c}")
+
+# update()
+set_a |= set_b | set_c
+print(f"{set_a} -- update()")
+
+# intersection_update()
+set_b &= set_a & set_c
+print(f"{set_b} -- intersection_update()")
+
+# difference_update()
+set_c -= set_b - set_a
+print(f"{set_c} -- difference_update()")
+
+# symmetric_difference_update()
+set_a ^= set_b ^ set_c
+print(f"{set_a} -- symmetric_difference_update()")
+
+
